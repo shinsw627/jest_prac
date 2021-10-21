d0a508575e15b5b48fc85b46a3048b85df76f17f
shinsw627 <shinsw627@naver.com>
2021-10-22 00:28:50 +0900

    기본 jest메소드 연습

--- fn.js
+++ fn.js
@@ -2,7 +2,12 @@ const fn = {
   add: (num1, num2) => {
     return num1 + num2;
   },
-  makeUser: (name, age) => ({ name, age }),
+
+  makeUser: (name, age) => ({ name, age, gender: undefined }),
+
+  throwErr: () => {
+    throw new Error("xx");
+  },
 };
 
 module.exports = fn;
--- fn.test.js
+++ fn.test.js
@@ -1,4 +1,5 @@
 const { test, expect } = require("@jest/globals");
+const exp = require("constants");
 const fn = require("./fn");
 
 test("1은 1이다.", () => {
@@ -17,11 +18,9 @@ test("2 더하기 3은 5다.", () => {
   expect(fn.add(2, 3)).toEqual(5);
 });
 
-test("이름과 나이를 전달 받아 객체를 반환한다.", () => {
-  expect(fn.makeUser("Mike", 30)).toBe({
-    name: "Mike",
-    age: 30,
-  });
+//toBeCloseTo 소수 계산 비교
+test("0.1 더하기 0.2는 0.3이다.", () => {
+  expect(fn.add(0.1, 0.2)).toBeCloseTo(0.3);
 });
 
 test("이름과 나이를 전달 받아 객체를 반환한다.", () => {
@@ -35,5 +34,61 @@ test("이름과 나이를 전달 받아 객체를 반환한다.", () => {
   expect(fn.makeUser("Mike", 30)).toStrictEqual({
     name: "Mike",
     age: 30,
+    gender: undefined,
   });
+});
+
+// toBeNull
+test("null은 null입니다.", () => {
+  expect(null).toBeNull();
+});
+
+// toBeUndefined
+test("undefined를 판별합니다.", () => {
+  expect(undefined).toBeUndefined();
+});
+
+// toBeDefined
+test("일반적인 문자열은 Defined 상태입니다.", () => {
+  expect("hello").toBeDefined();
+});
+
+// toBeTruthy
+test("0은 false 입니다.", () => {
+  expect(fn.add(1, -1)).toBeFalsy();
+});
+// toBeFalsy
+test("비어있지 않은 문자열은 true 이다.", () => {
+  expect(fn.add("hello", "world")).toBeTruthy();
+});
+
+//toBeGreaterThan 크다
+//toBeGreaterThanOrEqual 크거나 같다.
+//toBeLessThan 작다
+//toBeLessThanOrEqual 작거나 같다.
+test("ID는 10자 이하여야 한다", () => {
+  const id = "shinsw627";
+  expect(id.length).toBeLessThanOrEqual(10);
+});
+
+//toBe 일치
+test("비밀번호는 4자리만 가능하다.", () => {
+  const pw = "1234";
+  expect(pw.length).toBe(4);
+});
+
+//toMatch 문자열 확인
+test("Hello World 에 h라는 글자가 있는지 확인", () => {
+  expect("Hello World").toMatch(/h/i);
+});
+
+// toContain
+test("유저 리스트에 Mike가 있는가?", () => {
+  const user = "Mike";
+  const userList = ["Tom", "Jane", "Kai", "Mike"];
+  expect(userList).toContain(user);
+});
+
+test("에러가 나는지 확인", () => {
+  expect(() => fn.throwErr()).toThrow("xx");
 });
